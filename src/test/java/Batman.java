50741153365a6adb7ceb0b0e2b71bcc3d5919441
kulmurzaeva84 <kjika84@gmail.com>
2020-11-01 09:55:08 -0600

    updated2 Batman

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -3,5 +3,6 @@ public class Batman {
     public static void main(String[] args) {
         System.out.println("I will catch you Joker");
         System.out.println("Updated");
+        // yes he will catch him
     }
 }
